456d25e060968aa4f94b67446f7c824e019bf619
BOBSTEIN-rgb <igor.brodowski1234@gmail.com>
2026-09-09 22:25:46 +0200

    Add files via upload

---
+++ kuku.py
@@ -0,0 +1,27 @@
+# a = float(input())
+# print(f"ppkwadrat  {a} *{a} = {a * a}")
+
+# a = float(input())
+# b = float(input())
+# print(f"ppprostokat  {a} *{b} = {a * b}")
+
+# a = float(input())
+# b = float(input())
+# h= float(input())
+# print(f"pptrapez  ({a} + {b}) * {h} / 2 = {(a + b) * h / 2}")
+
+# a = float(input())
+# h = float(input())
+# print(f"pptrojkat  {a} * {h} / 2 = {(a * h) / 2}")
+
+# import math
+# PI = math.pi
+# r = float(input())
+# print(f"ppkolo  {PI} * {r}^2 = {PI * r ** 2}")
+
+# a = float(input())
+# b = float(input())
+# c = float(input())
+# print(f"Vszescian  {a} * {b} * {c} = {a * b * c}")
+
+print("kuku")
